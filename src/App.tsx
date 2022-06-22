0210f19121bffcdf9a89773f8fea07be2219e950
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-06-22 15:03:33 -0300

    feat: create routes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CreateCar from "./pages/CreateCar"
+import EditCar from "./pages/EditCar"
+import Home from "./pages/Home"
 
 
 function App() {
 
   return (
     <div>
-      Cars
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/create" element={<CreateCar/>}/>
+          <Route path="/edit/*" element={<EditCar/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/EditCar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EditCar() {
+  return (
+    <div>Edit Car</div>
+  )
+}
