46a807935c2c0c16ef981be04e265b70d2fc8472
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-06-22 23:55:06 -0300

    feat: create global types file

---
+++ src/types/globalTypes.ts
@@ -0,0 +1,12 @@
+export type Car = {
+    id: number;
+    placa: string;
+    chassi: string;
+    renovam: number;
+    ano_modelo: string;
+    ano_fabricacao: string;
+    cor: string;
+    uf: string;
+    path_img: string;
+
+}
